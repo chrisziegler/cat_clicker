98a7939045cadc2cc5e9ce953f1b38ab015decbb
Chris <chrisrziegler@gmail.com>
2016-06-27 13:51:28 -0700

    added an extra cat to cat clicker - practice in code revisions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 //cat clicker functionality here
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    var counter = 0;
-    var el = document.getElementById('cat');
+    var counter = 0, counter2 = 0;
+    var cat = document.getElementById('cat');
+    var catName = document.querySelector('.cat-name');
+    var cat2 = document.getElementById('cat2');
+    var cat2Name = document.querySelector('.cat2-name');
     /*The following could also be coded:
       el.addEventlistener("click", myFunction);
       function myFunction() {
@@ -10,18 +13,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
     */
 
-    el.addEventListener("click", function() {
+// add cat names w/code
+   catName.innerText = 'Stash';
+   cat2Name.innerText = 'Food Eater';
+    cat.addEventListener("click", function() {
         counter++;
       var updateCount = document.getElementById('count');
       updateCount.innerHTML = '<span>' + 'You clicked on me! I\'ve now been clicked this many times: &nbsp' + '<b>' + counter + '</b>' + '</span>';
-
-        console.log(updateCount);
-
-
-
-
-
-
     });
-
+    cat2.addEventListener("click", function() {
+      counter2++;
+      var updateCount2 = document.getElementById('count2');
+      updateCount2.innerHTML = '<span>' + 'Knew I was better than that other cat! I\'ve now been clicked this many times: &nbsp' + '<b>' + counter2 + '</b>' + '</span>';
+    })
 });
