2791e0159ef3fc4ce146c7291948d730cf91e219
Chris <chrisrziegler@gmail.com>
2016-06-28 11:51:49 -0700

    initial commit of Cat Clicker Premium Gold

---
+++ premium/js/script.js
@@ -0,0 +1,70 @@
+
+
+  document.addEventListener('DOMContentLoaded', function(event){
+        var cats = ['Charlie', 'Muffin', 'Stash', 'Food Eater', 'Groundskeeper Willy', 'Mike'];
+
+        for(var i = 0; i < cats.length; i++) {
+          var dynamicCats = cats[i];
+          var catDiv = document.createElement('div');
+          catDiv.textContent = dynamicCats;
+
+      catDiv.addEventListener('click', (function(catsCopy){
+       return function() {
+         //replace with load selected cat into #clicker
+         alert(catsCopy);
+       };
+
+
+     })(dynamicCats));
+
+document.getElementById('catlist').appendChild(catDiv);
+
+  };
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     var counter = 0, counter2 = 0;
+//     var cat = document.getElementById('cat');
+//     var catName = document.querySelector('.cat-name');
+//     var cat2 = document.getElementById('cat2');
+//     var cat2Name = document.querySelector('.cat2-name');
+//     /*The following could also be coded:
+//       el.addEventlistener("click", myFunction);
+//       function myFunction() {
+//         document.getElementById('count').innerHTML = "You clicked me!";
+//     }
+//     */
+//
+// // add cat names w/code
+//    catName.innerText = 'Stash';
+//    cat2Name.innerText = 'Food Eater';
+//     cat.addEventListener("click", function() {
+//         counter++;
+//       var updateCount = document.getElementById('count');
+//       updateCount.innerHTML = '<span>' + 'You clicked on me! I\'ve now been clicked this many times: &nbsp' + '<b>' + counter + '</b>' + '</span>';
+//     });
+//     cat2.addEventListener("click", function() {
+//       counter2++;
+//       var updateCount2 = document.getElementById('count2');
+//       updateCount2.innerHTML = '<span>' + 'Knew I was better than that other cat! I\'ve now been clicked this many times: &nbsp' + '<b>' + counter2 + '</b>' + '</span>';
+//     })
+// });
